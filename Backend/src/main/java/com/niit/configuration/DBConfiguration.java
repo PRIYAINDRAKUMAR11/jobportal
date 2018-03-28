3720a4f111f9de85a37310d78d5885b9e317b32e
priyaindrakumar11 <priyaindrakumar11@gmail.com>
2018-03-28 10:19:26 +0530

    priya

--- Backend/src/main/java/com/niit/configuration/DBConfiguration.java
+++ Backend/src/main/java/com/niit/configuration/DBConfiguration.java
@@ -15,9 +15,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import com.niit.model.BlogComment;
 import com.niit.model.BlogPost;
 import com.niit.model.BlogPostLikes;
+import com.niit.model.Friend;
 import com.niit.model.Job;
 import com.niit.model.Notification;
+import com.niit.model.ProfilePicture;
 import com.niit.model.User;
+import com.niit.model.Chat;
 
 
 @Configuration
@@ -36,7 +39,7 @@ public class DBConfiguration {
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
 		lsf.addProperties(hibernateProperties);
-		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,Notification.class,BlogPostLikes.class,BlogComment.class};//class objects of all entities
+		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,Notification.class,BlogPostLikes.class,BlogComment.class,ProfilePicture.class,Friend.class,Chat.class};//class objects of all entities
 	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
 	}
 	@Bean
